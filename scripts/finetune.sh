55f2a567fdb841590c27b960ad447bac1ecfc9cc
wadeKeith <2470887663@qq.com>
2025-03-23 18:55:09 +0800

    modified finetune and some default setting of params

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ -d "wandb" ]; then
+    rm -rf wandb
+    echo "Wandb dir clean"
+else
+    echo "Wandb dir doesn't exists"
+fi
 
 # You can use 2B instead of 7B
 # MODEL_NAME="Qwen/Qwen2-VL-7B-Instruct"
@@ -11,7 +17,25 @@ BATCH_PER_DEVICE=4
 NUM_DEVICES=4
 GRAD_ACCUM_STEPS=$((GLOBAL_BATCH_SIZE / (BATCH_PER_DEVICE * NUM_DEVICES)))
 
+export OUTPUT_DIR="./checkpoints/3b/libero"
+export CFLAGS="-I/usr/include"
+export LDFLAGS="-L/usr/lib/x86_64-linux-gnu"
+export CUTLASS_PATH="/home/yin/cutlass/"
 export PYTHONPATH=src:$PYTHONPATH
+
+
+if [ ! -d "$OUTPUT_DIR" ]; then
+    mkdir "$OUTPUT_DIR"
+    echo "Folder '$OUTPUT_DIR' created"
+else
+    echo "Folder '$OUTPUT_DIR' already exists"
+fi
+
+
+export WANDB_PROJECT="qwen-2.5-vl-libero"
+export WANDB_MODE="online"
+export WANDB_NAME="3b-fft-250323"
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 deepspeed src/training/train.py \
     --use_liger True \
@@ -23,11 +47,12 @@ deepspeed src/training/train.py \
     --freeze_vision_tower False \
     --freeze_llm False \
     --tune_merger True \
+    --lora_enable False \
     --bf16 True \
     --fp16 False \
     --disable_flash_attn2 False \
-    --output_dir output/fft_0912 \
-    --num_train_epochs 1 \
+    --output_dir $OUTPUT_DIR \
+    --num_train_epochs 10 \
     --per_device_train_batch_size $BATCH_PER_DEVICE \
     --gradient_accumulation_steps $GRAD_ACCUM_STEPS \
     --image_min_pixels $((512 * 28 * 28)) \
@@ -41,9 +66,12 @@ deepspeed src/training/train.py \
     --logging_steps 1 \
     --tf32 True \
     --gradient_checkpointing True \
-    --report_to tensorboard \
+    --report_to wandb \
     --lazy_preprocess True \
-    --save_strategy "steps" \
-    --save_steps 200 \
+    --save_strategy "epoch" \
     --save_total_limit 10 \
-    --dataloader_num_workers 4
+    --dataloader_num_workers 32
+
+
+#    --save_strategy "steps"
+#    --save_steps 1 \
--- src/training/params.py
+++ src/training/params.py
@@ -11,13 +11,14 @@ class ModelArguments:
 
 @dataclass
 class TrainingArguments(TrainingArguments):
+    cache_dir: Optional[str] = field(default=None)
     optim: str = field(default="adamw_torch")
     adam_beta1: float = field(default=0.9)
     adam_beta2: float = field(default=0.999)
     adam_epsilon: float = field(default=1e-8)
 
     freeze_vision_tower: bool = field(default=False)
-    freeze_llm: bool = field(default=True)
+    freeze_llm: bool = field(default=False)
     tune_merger: bool = field(default=True)
     disable_flash_attn2: bool = field(default=False)
 
@@ -41,41 +42,41 @@ class TrainingArguments(TrainingArguments):
         default=16,
         metadata={"help": "How many bits to use."}
     )
-    lora_enable: bool = True
+    lora_enable: bool = False
     vision_lora: bool = False
     use_dora: bool = False
     lora_rank: int = 64
-    lora_alpha: int = 64
+    lora_alpha: int = 16
     lora_dropout: float = 0.05
     lora_weight_path: str = ""
     lora_bias: str = "none"
     vision_lr: Optional[float] = 2e-6
     merger_lr: Optional[float] = 1e-5
-    lora_namespan_exclude: str = field(default="['lm_head', 'embed_tokens']", metadata={"help": "List of namespan to exclude for LoRA"})
+    lora_namespan_exclude: str = field(default=None, metadata={"help": "List of namespan to exclude for LoRA"})
     num_lora_modules: int = -1
     use_liger: bool = True
 
-    # TrainingArguments inherit arguments
-    remove_unused_columns: Optional[bool] = field(default=False)
-    bf16: bool = field(default=True)
-    fp16: bool = field(default=False)
-    output_dir: str = field(default="output/testing_lora")
-    num_train_epochs: float = field(default=1)
-    per_device_train_batch_size: int = field(default=1)
-    gradient_accumulation_steps: int = field(default=1)
-    learning_rate: float = field(default=1e-4)
-    weight_decay: float = field(default=0.1)
-    warmup_ratio: float = field(default=0.03)
-    lr_scheduler_type: str = field(default="cosine")
-    logging_steps: float = field(default=1)
-    tf32: Optional[bool] = field(default=True)
-    gradient_checkpointing: bool = field(default=True)
-    report_to: str = field(default="wandb")
-    save_strategy: str = field(default="steps")
-    save_steps: float = field(default=200)
-    save_total_limit: Optional[int] = field(default=10)
-    dataloader_num_workers: int = field(default=12)
-    # deepspeed: str = field(default="scripts/zero2.json")
+    # # TrainingArguments inherit arguments
+    # remove_unused_columns: Optional[bool] = field(default=False)
+    # bf16: bool = field(default=True)
+    # fp16: bool = field(default=False)
+    # output_dir: str = field(default="output/testing_lora")
+    # num_train_epochs: float = field(default=1)
+    # per_device_train_batch_size: int = field(default=1)
+    # gradient_accumulation_steps: int = field(default=1)
+    # learning_rate: float = field(default=1e-4)
+    # weight_decay: float = field(default=0.1)
+    # warmup_ratio: float = field(default=0.03)
+    # lr_scheduler_type: str = field(default="cosine")
+    # logging_steps: float = field(default=1)
+    # tf32: Optional[bool] = field(default=True)
+    # gradient_checkpointing: bool = field(default=True)
+    # report_to: str = field(default="wandb")
+    # save_strategy: str = field(default="steps")
+    # save_steps: float = field(default=200)
+    # save_total_limit: Optional[int] = field(default=10)
+    # dataloader_num_workers: int = field(default=12)
+    # # deepspeed: str = field(default="scripts/zero2.json")
     
 
 
